d15558db0e573486da7713608206d85b7838dbeb
roddajohn <johnrod.john@gmail.com>
2016-10-17 11:33:59 -0400

    updated

---
+++ main.py
@@ -0,0 +1,29 @@
+import sqlite3 
+import csv
+
+name = "supreme-dollop.db"
+
+db = sqlite3.connect(name)
+c = db.cursor()
+
+statement = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER)"
+c.execute(statement)
+
+obj = open("peeps.csv")
+d = csv.DictReader(obj)
+
+for k in d:
+    p = 'INSERT INTO students VALUES ("' + k['name'] +  '", "' + k['age'] +  '", "' + k['id'] + '" )'
+    c.execute(p)
+    
+statement = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)"
+c.execute(statement)
+obj = open("courses.csv")
+d = csv.DictReader(obj)
+for k in d:
+    p = 'INSERT INTO courses VALUES ("' + k['code'] +  '", "' + k['mark'] +  '", "' + k['id'] + '" )'
+    c.execute(p)
+    db.commit()
+db.close()
+
+                        
